b64354afd5e25d0d1505ef348a253741d67a4908
Malicious-Bunny <ngohgeorge.mbahnji@ictuniversity.edu.cm>
2023-11-22 00:54:51 +0100

    Fixed pdf sizes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      //externalize react tag input component
+      external: ['react-icons/fa', 'react-icons/md', 'react-icons/ai', 'react-icons/bs', 'react-icons/go', 'react-icons/im', 'react-icons/io', 'react-icons/ti', 'react-icons/vsc', 'react-icons/fi', 'react-icons/gi', 'react-icons/gr', 'react-icons/hi', 'react-icons/io5', 'react-icons/ri', 'react-icons/si', 'react-icons/tw', 'react-icons/wi', 'react-icons', 'reactjs-tag-input'],
+    },
+  },
 })
